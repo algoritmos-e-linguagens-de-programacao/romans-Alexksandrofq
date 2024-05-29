4134a9598d1e2178d2be23fda76892e9f032c04f
Alexksandro Fq <168040540+Alexksandrofq@users.noreply.github.com>
2024-05-28 22:08:12 -0300

    primeira alteração aula 28/05/24

--- 01/romans.py
+++ 01/romans.py
@@ -1,6 +1,17 @@
 def int_to_roman(num):
     # Implemente sua função aqui
-    pass
+   
+    if not 1 <= num <= 3999:
+        return
+    valores = (1000, 'M'), (500, 'D'), (100, 'C'), (50, 'L'), (10, 'X'), (5, 'V'), (1, 'I')
+    resultado = ''
+
+    for valor, romano in valores:
+        while num >= valor:
+            resultado += romano 
+            num -=valor
+
+print (int_to_roman(num))
 
 
 def roman_to_int(s):
